3d0c320a722f48da987c8105938b14548c9a2818
Nafiz Shahriar <shahriarsami123@gmail.com>
2025-05-31 01:41:45 +0600

    FruitNode.java added

---
+++ CSE220/Lab04/Task2/FruitNode.java
@@ -0,0 +1,10 @@
+public class FruitNode {
+    Object[] fruit = new Object[2];
+    FruitNode next;
+
+    public FruitNode(String name, Integer price){
+        fruit[0]=name;
+        fruit[1]=price;
+    }
+    
+}
